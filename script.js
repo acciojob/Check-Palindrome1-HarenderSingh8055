e364b4bcc48c9f66f93f7428df1cc81af31612f4
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-01 21:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
+	var newStr = str.split(" ");
+	var resultStr = newStr.join("");
 
+	for(var i =0;i<resultStr.length;i++){
+		if(resultStr[i] !==  resultStr[resultStr.length - 1 - i]){
+			return false;
+		}
+	}
+	return true;
+	
 }
 module.exports = palindrome
